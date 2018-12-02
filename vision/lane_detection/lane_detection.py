1171c1f2e568e7dd4fa1466e4fbe421342fd95d9
Anshul <anshuls3@illinois.edu>
2018-12-02 13:13:34 -0600

    Removed duplicates

--- vision/lane_detection/lane_detection.py
+++ vision/lane_detection/lane_detection.py
@@ -123,62 +123,33 @@ def draw_lines(img, lines, color = [0, 255, 0], thickness = 3, slope_threshold =
     poly_left = np.poly1d([left_m, left_b])
     poly_right = np.poly1d([right_m, right_b])
 
-    print(poly_left)
-    print(poly_right)
-    # just below our triangular cropped image
-    min_y = int(img.shape[0] * (3/5))
-    max_y = int(img.shape[0])
+    # print(poly_left)
+    # print(poly_right)
+    # # just below our triangular cropped image
+    # min_y = int(img.shape[0] * (3/5))
+    # max_y = int(img.shape[0])
 
-    left_x_start = int(poly_left(max_y))
-    left_x_end = int(poly_left(min_y))
+    # left_x_start = int(poly_left(max_y))
+    # left_x_end = int(poly_left(min_y))
 
-    right_x_start = int(poly_right(max_y))
-    right_x_end = int(poly_right(min_y))
+    # right_x_start = int(poly_right(max_y))
+    # right_x_end = int(poly_right(min_y))
 
-    mask = np.zeros_like(img)
+    # mask = np.zeros_like(img)
 
-    cv2.line(img, (left_x_start,max_y), (left_x_end, min_y), color, thickness)
-    cv2.line(img, (right_x_start,max_y), (right_x_end, min_y), color, thickness)
+    # cv2.line(img, (left_x_start,max_y), (left_x_end, min_y), color, thickness)
+    # cv2.line(img, (right_x_start,max_y), (right_x_end, min_y), color, thickness)
 
-    return img
+    return poly_left, poly_right
 
-def main():
-    vid = cv2.VideoCapture('clip_highway_video.mp4')
-    frame_width = int(vid.get(3))
-    frame_height = int(vid.get(4))
-    out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
-    while vid.isOpened():
+def main(frame):
+    # vid = cv2.VideoCapture('clip_highway_video.mp4')
+    # frame_width = int(vid.get(3))
+    # frame_height = int(vid.get(4))
+    # out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (frame_width,frame_height))
+    # while vid.isOpened():
 
-        ret, frame = vid.read()
-
-        gray = grayscale(frame)
-
-        canny_image = canny_edge(gray)
-
-        width = canny_image.shape[1]
-        height = canny_image.shape[0]
-
-        region = [(0,0), (int(width/2),int(height/2)), (width,height)]
-        mask_image = image_mask(canny_image, region)
-
-        lines = hough_transform(mask_image)
-        for i in range(len(lines)):
-            cv2.line(frame,(lines[i][0][0], lines[i][0][1]),(lines[i][0][2], \
-                    lines[i][0][3]),(0,255,0), 3)
-        for x1,y1,x2,y2 in lines[0]:
-            cv2.line(mask_image,(x1,y1),(x2,y2),(0,255,0),10)
-        cv2.imshow('frame',frame)
-        line_image = draw_lines(mask_image,lines)
-        line_image = draw_lines(frame, lines)
-        out.write(line_image)
-        cv2.imshow('line_image',line_image)
-        cv2.waitKey(1)
-
-    vid.release()
-    out.release()
-    cv2.destrolAllWindows()
-
-def main2(frame):
+        # ret, frame = vid.read()
 
     gray = grayscale(frame)
 
@@ -191,18 +162,22 @@ def main2(frame):
     mask_image = image_mask(canny_image, region)
 
     lines = hough_transform(mask_image)
-    toReturn = []
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-        toReturn.append([x1,y1,x2,y2])
-    return toReturn
+        # for i in range(len(lines)):
+        #     cv2.line(frame,(lines[i][0][0], lines[i][0][1]),(lines[i][0][2], \
+        #             lines[i][0][3]),(0,255,0), 3)
+        # for x1,y1,x2,y2 in lines[0]:
+        #     cv2.line(mask_image,(x1,y1),(x2,y2),(0,255,0),10)
+        # line_image = draw_lines(mask_image,lines)
+        
+        # line_image, poly_left, poly_right = draw_lines(frame, lines)
+    return draw_lines(frame, lines)
+        
+        # cv2.waitKey(1)
+
+    # vid.release()
+    # out.release()
+    # cv2.destrolAllWindows()
+
 
 
 
